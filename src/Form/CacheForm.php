a4dd186522713de02c0d0bba362f6472fb2b5ab9
John van Breda <john.vanbreda@biodiverseit.co.uk>
2015-08-05 21:55:26 +0100

    Indicia cache management config page

--- src/Form/CacheForm.php
+++ src/Form/CacheForm.php
@@ -23,34 +23,45 @@ class CacheForm extends FormBase {
    */
   public function buildForm(array $form, \Drupal\Core\Form\FormStateInterface $form_state) {
     $form = array();
+    $form['instruction'] = array(
+      '#markup' => '<p>' . t('When Indicia requests data from the Warehouse, it can cache a local copy of ' .
+          'these data to help speed up future requests for the same data. Although this makes a significant ' .
+          'improvement to your website\'s performance, it can mean that changes to data are not visible ' .
+          'on your website for several hours. Clear the cache to ensure that the latest copy of all data ' .
+          'is loaded.') . '</p>'
+    );
+    $query = \Drupal::entityQuery('node')->condition('type', 'iform_page')->range(0, 1);
+    $nids = $query->execute();
+    if (count($nids)) {
+      $nid = array_pop($nids);
+      global $base_url;
+      $url = $base_url . \Drupal::service('path.alias_manager')->getAliasByPath("/node/$nid");
+      $urlWithoutCache = $url . (strpos($url, '?') === FALSE ? '?' : '&') . 'nocache';
+      $example = ' ' . t('For example, you can change the URL <a href="!url">!url</a> to ' .
+          '<a href="!urlWithoutCache">!urlWithoutCache</a> to render the page without using the Indicia cache.',
+          array('!url' => $url, '!urlWithoutCache' => $urlWithoutCache));
+    } else {
+      $example = '--';
+    }
+    $form['nocache'] = array(
+      '#markup' => '<p>' . t('If you want to test changes to an Indicia page without forcing a hard-reset ' .
+          'of the entire cache, then you can add a parameter called <strong>nocache</strong> to the URL of the page to reload ' .
+          'it without using the Indicia cache.') . $example . '</p>'
+    );
+    $form['submit'] = array(
+      '#type' => 'submit',
+      '#value' => t('Clear Indicia cache'),
+    );
     return $form;
   }
 
   /**
    * {@inheritdoc}
    */
-  public function validateForm(array &$form, \Drupal\Core\Form\FormStateInterface $form_state) {
-
-  }
-
-  /**
-   * {@inheritdoc}
-   */
   public function submitForm(array &$form, \Drupal\Core\Form\FormStateInterface $form_state) {
-
-  }
-
-  /**
-   * Utility function to populate the list of warehouses in the global $_iform_warehouses. Each warehouse is loaded from an .inc
-   * file in the warehouses sub-folder
-   * @todo Remove need for global
-   */
-  private function load_warehouse_array() {
-    global $_iform_warehouses;
-    $_iform_warehouses = array();
-    foreach (glob(drupal_get_path('module', 'iform') . '/warehouses/*.inc') as $warehouse_file) {
-      require($warehouse_file);
-    }
+    iform_load_helpers(array('data_entry_helper'));
+    \data_entry_helper::clear_cache();
+    drupal_set_message(t('The Indicia cache has been cleared.'), 'status');
   }
 
 }
