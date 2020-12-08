e774c228bdb1fecadac2ac029d6e5e871d5a0844
John van Breda <john.vanbreda@biodiverseit.co.uk>
2020-12-08 15:55:12 +0000

    Drupal 8 support for group join link shares

--- src/Controller/IformController.php
+++ src/Controller/IformController.php
@@ -5,8 +5,29 @@ namespace Drupal\iform\Controller;
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\HttpFoundation\Response;
 use Drupal\node\Entity\Node;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class IformController extends ControllerBase {
+
+  /**
+   * Messenger service.
+   */
+  protected $messenger;
+
+  /**
+   * Dependency inject services.
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      // Load the service required to construct this class.
+      $container->get('messenger')
+    );
+  }
+
+  public function __construct($messenger) {
+    $this->messenger = $messenger;
+  }
 
   /**
    * Prebuilt form ajax function callback.
@@ -28,7 +49,7 @@ class IformController extends ControllerBase {
    */
   public function ajaxCallback($form, $method, $nid) {
     if ($form === NULL || $method === NULL || $nid === NULL) {
-      return t('Incorrect AJAX call');
+      return $this->t('Incorrect AJAX call');
     }
     $class = "\iform_$form";
     $method = "ajax_$method";
@@ -105,6 +126,85 @@ class IformController extends ControllerBase {
     require_once \iform_client_helpers_path() . 'DynamicAttrsProxyHelper.php';
     \DynamicAttrsProxyHelper::callMethod($method);
     return new Response('', http_response_code());
+  }
+
+  /**
+   * Callback for shared group join links.
+   * 
+   * @param string $title
+   *   URL formatted name of the group.
+   * @param string $parentTitle
+   *   Optional URL formatted name of the parent group.
+   */
+  public function joinGroupCallback($title, $parentTitle = NULL) {
+    iform_load_helpers(['report_helper']);
+    $config = \Drupal::config('iform.settings');
+    $auth = \report_helper::get_read_write_auth($config->get('website_id'), $config->get('password'));
+    $indiciaUserId = hostsite_get_user_field('indicia_user_id', 0);
+    $params = array(
+      'title' => $title,
+      'currentUser' => $indiciaUserId
+    );
+    if ($parentTitle)
+      $params['parent_title'] = $parentTitle;
+    // Look up the group.
+    $groups = \report_helper::get_report_data(array(
+      'dataSource' => 'library/groups/find_group_by_url',
+      'readAuth' => $auth['read'],
+      'extraParams' => $params
+    ));
+    if (isset($groups['error'])) {
+      $this->messenger->addWarning($this->t('An error occurred when trying to access the group.'));
+      \Drupal::logger('iform')->notice('Groups page load error: ' . var_export($groups, true));
+      hostsite_goto_page('<front>');
+      return;
+    }
+    if (!count($groups)) {
+      $this->messenger->addWarning($this->t('The group you are trying to join does not appear to exist.'));
+      throw new NotFoundHttpException();
+      return;
+    }
+    if (count($groups) > 1) {
+      $this->messenger->addWarning($this->t('The group you are trying to join has a duplicate name with another group so cannot be joined in this way.'));
+      hostsite_goto_page('<front>');
+    }
+    $group = $groups[0];
+    if ($group['member']==='t') {
+      $this->messenger->addMessage($this->t("Welcome back to the @group.", ['@group' => iform_readable_group_title($group)]));
+      return iform_show_group_page($group, $auth['read']);
+    }
+    elseif ($group['joining_method_raw']==='I') {
+      $this->messenger->addWarning($this->t('The group you are trying to join is private.'));
+      hostsite_goto_page('<front>');
+      return;
+    }
+    global $user;
+    if ($user->uid) {
+      $r = '';
+      // User is logged in
+      if (!$indiciaUserId) {
+        $this->messenger->addMessage($this->t("Before joining $group[title], please set your surname on your user account profile."));
+        hostsite_goto_page('<front>');
+        return;
+      }
+      elseif ($group['pending']==='t' && $group['joining_method'] !== 'P') {
+        // Membership exists but is pending.
+        $this->messenger->addMessage($this->t('Your application to join @group is still waiting for a group administrator to approve it.', array('@group' => iform_readable_group_title($group))));
+      }
+      elseif (!isset($_GET['confirmed'])) {
+        $r .= _iform_group_confirm_form($group);
+      }
+      elseif (!iform_join_public_group($group, $auth['write_tokens'], $indiciaUserId)) {
+        hostsite_goto_page('<front>');
+        return;
+      }
+      $r .= iform_show_group_page($group, $auth['read']);
+      return $r;
+    }
+    else {
+      // User is not logged in, so redirect to login page with parameters so we know which group
+      hostsite_goto_page('user', ['group_id' => $group['id'], 'destination' => $_GET['q']]);
+    }
   }
 
 }
