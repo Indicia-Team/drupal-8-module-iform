16c7b8cd556b0dba66c82833aecee0d6118d3392
John van Breda <john.vanbreda@biodiverseit.co.uk>
2022-01-04 12:42:06 +0000

    Implement hostsite_set_breadcrumb for D8.

---
+++ src/Breadcrumb/IformBreadcrumbBuilder.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Drupal\iform\Breadcrumb;
+
+use Drupal\Core\Breadcrumb\Breadcrumb;
+use Drupal\Core\Breadcrumb\BreadcrumbBuilderInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\Link;
+use Drupal\Core\Url;
+
+/**
+ * Breadcrumb builder for calls to hostsite_set_breadcrumb().
+ */
+class IformBreadcrumbBuilder implements BreadcrumbBuilderInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function applies(RouteMatchInterface $route_match) {
+    // Determine if the current page is a node page with iform code that called
+    // hostsite_set_breadcrumb.
+    $node = $route_match->getParameter('node');
+    if ($node && class_exists('helper_base') && !empty(\helper_base::$breadcrumb)) {
+      return TRUE;
+    }
+    return FALSE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(RouteMatchInterface $route_match) {
+    $breadcrumb = new Breadcrumb();
+    if (class_exists('helper_base') && !empty(\helper_base::$breadcrumb)) {
+      $breadcrumb->addLink(Link::createFromRoute('Home', '<front>'));
+      foreach (\helper_base::$breadcrumb as $path => $caption) {
+        if (substr($path, 0, 1) !== '/' && substr($path, 0, 1) !== '<') {
+          $path = "/$path";
+        }
+        if ($caption === '#currentPageTitle#') {
+          $nid = 0;
+          $node = \Drupal::routeMatch()->getParameter('node');
+          if ($node) {
+            $nid = $node->id();
+          }
+          $caption = hostsite_get_page_title($nid);
+        }
+        if (substr($path, 0, 1) === '/') {
+          foreach ($_GET as $key => $value) {
+            // GET parameters can be used as replacements.
+            $path = str_replace("#$key#", $value, $path);
+          }
+          $breadcrumb->addLink(Link::fromTextAndUrl($caption, Url::fromUserInput($path)));
+        }
+        else {
+          // Handle routes such as <front>, <none>.
+          $breadcrumb->addLink(Link::createFromRoute($caption, $path));
+        }
+      }
+    }
+    $breadcrumb->addCacheContexts(['route']);
+    // Return object of type breadcrumb.
+    return $breadcrumb;
+  }
+
+}
